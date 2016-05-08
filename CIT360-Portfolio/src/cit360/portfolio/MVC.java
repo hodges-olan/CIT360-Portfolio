c4c35275885f739892ff7e0ce76d800fb806415e
co075oh <co075oh@W5136980.ena.us.experian.local>
2016-05-07 21:45:55 -0500

    Merge origin/master
    Conflicts:
    	theRevengeOfMerek/src/byui/cit260/theRevengeOfMerek/view/ArtifactQuestView.java

---
+++ CIT360-Portfolio/src/cit360/portfolio/MVC.java
@@ -0,0 +1,18 @@
+/*
+ * MVC (Model, View, Control)
+ * Brandon Urednick presented over MVC this week and explained the purpose behind MVC.
+ * He used the link at http://www.w3schools.com/aspnet/mvc_intro.asp to show us the
+ * meaning behind each of the layers.  I can see how this is a popular method, as it
+ * allows you to break down the basic functionality of most programs to this separate
+ * sections.  This was the same way we split up the game that we created in CIT-260,
+ * so it was somewhat familiar to most of us.
+ */
+package cit360.portfolio;
+
+/**
+ *
+ * @author co075oh
+ */
+public class MVC {
+
+}
