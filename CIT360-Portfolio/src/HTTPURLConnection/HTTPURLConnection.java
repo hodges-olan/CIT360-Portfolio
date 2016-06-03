a25dcaa94b351c4cedebe2a8eaaa60d5a20d7fc2
co075oh <co075oh@W5136980.ena.us.experian.local>
2016-06-02 19:33:11 -0500

    Merge origin/master

--- CIT360-Portfolio/src/HTTPURLConnection/HTTPURLConnection.java
+++ CIT360-Portfolio/src/HTTPURLConnection/HTTPURLConnection.java
@@ -5,10 +5,41 @@
  */
 package HTTPURLConnection;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  *
  * @author co075oh
  */
 public class HTTPURLConnection {
-    
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("http://www.vogella.com");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            String readStream = readStream(con.getInputStream());
+            // Give output for the command line
+            System.out.println(readStream);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static String readStream(InputStream in) {
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(in));) {
+      
+            String nextLine;
+            while ((nextLine = reader.readLine()) != null) {
+                sb.append(nextLine);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return sb.toString();
+    }
 }
